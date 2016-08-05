95e40ab75850a4c2f5da0cbc5203dd6161cd3675
Kyrene Jackson <ky.jackson01@gmail.com>
2016-08-05 13:46:36 -0500

    Create user model, validations, and association

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+
+  validates_presence_of :username, :email, :encrypted_password
+
+  validates_uniqueness_of :username, :email
+
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+
+  has_many :rounds
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+end
