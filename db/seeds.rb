4f2260a9f179a3d6efafe076db0ac563b5005b15
Alex Stevenson <alexstevenson19@gmail.com>
2016-08-06 23:47:44 -0500

    update suds questions. still thinking of pet questions (unfortunately it seems Rear Gear has gone out of business)

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,14 @@ Card.create(question: "Which of these is a real social club activity?", correct_
 Card.create(question: "Which one was originally a wallpaper stain remover?",correct_answer_id: 7, deck_id: 1)
 
 # Suds =============================================================
-Card.create(question: "Why do some people eat small amounts of detergent?", correct_answer_id: 10, deck_id: 2)
+Card.create(question: "Which laundry soap started to sponsor 15 minute radio dramas in 1933?", correct_answer_id: 10, deck_id: 2)
 
 Card.create(question: "Who is Mr. Proper?", correct_answer_id: 13, deck_id: 2)
 # Mr. Clean (in Europe)
 # wrong answers, Mr. Sparkle (Japan),
 
-Card.create(question: "What is cleaning advice for lazy people?",correct_answer_id: 16, deck_id: 2)
+Card.create(question: "What does S.O.S. stand for, (as in the S.O.S. cleaning pads)?",correct_answer_id: 16, deck_id: 2)
+# Save our Saucepans
 # Eat food out of a tortilla to avoid washing bowls
 
 
@@ -42,9 +43,9 @@ PossibleAnswer.create(answer: "Sock Heaven", card_id: 1)
 
 PossibleAnswer.create(answer: "Cleaning public toilets by some Good Samaritans in Japan.", card_id: 2)
 
-PossibleAnswer.create(answer: "Cleaning sink drains by hand in Sweden.", card_id: 2)
+PossibleAnswer.create(answer: "Cleaning public sink drains by hand in Sweden.", card_id: 2)
 
-PossibleAnswer.create(answer: "Cleaning .....", card_id: 2)
+PossibleAnswer.create(answer: "Cleaning the wheels of shopping carts by high schoolers in Belarus.", card_id: 2)
 
 PossibleAnswer.create(answer: "Play Doh", card_id: 3)
 
@@ -52,11 +53,11 @@ PossibleAnswer.create(answer: "Silly Putty", card_id: 3)
 
 PossibleAnswer.create(answer: "Day-Glo Slime", card_id: 3)
 
-PossibleAnswer.create(answer: "Pica", card_id: 4)
+PossibleAnswer.create(answer: "Oxydol", card_id: 4)
 
-PossibleAnswer.create(answer: "Something else", card_id: 4)
+PossibleAnswer.create(answer: "Selox", card_id: 4)
 
-PossibleAnswer.create(answer: "Something else again", card_id: 4)
+PossibleAnswer.create(answer: "Lux", card_id: 4)
 
 PossibleAnswer.create(answer: "Mr. Clean (in Europe)", card_id: 5)
 
@@ -64,11 +65,11 @@ PossibleAnswer.create(answer: "Mr. Sparkle (in Japan)", card_id: 5)
 
 PossibleAnswer.create(answer: "Mr. Maple (in Canada)", card_id: 5)
 
-PossibleAnswer.create(answer: "Eat food out of a tortilla to avoid washing bowls", card_id: 6)
+PossibleAnswer.create(answer: "Save Our Saucepans", card_id: 6)
 
-PossibleAnswer.create(answer: "Something lazy", card_id: 6)
+PossibleAnswer.create(answer: "Save Our Silverware", card_id: 6)
 
-PossibleAnswer.create(answer: "Something else lazy", card_id: 6)
+PossibleAnswer.create(answer: "Save Our Sanity", card_id: 6)
 
 PossibleAnswer.create(answer: "Pet answer 1", card_id: 7)
 
